e657647694f61bb18cd205b8ce028b4d2dc9f276
mahfuzur <mdmahfuazur7788@gamil.com>
2022-10-21 11:24:01 +0600

    React Component used

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+
+//JSF Code
 
 function App() {
-  return (
+   return (
     <div className="App">
         <h1>React First App</h1>
+        <Some/>
     </div>
+  );
+ 
+}
+
+function Some() {
+  return(
+    <h1>Another Component React Project</h1>
   );
 }
 
