6334bb15584c868ac33c058dc6ab04a01281797a
mahfuzur <mdmahfuazur7788@gamil.com>
2022-10-21 11:38:01 +0600

    two rulas of JSX syntax

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import React from 'react';
 //JSF Code
 
 function App() {
-  //  return (
-  //   <div className="App">
-  //       <h1>React First App</h1>
-  //       <Some/>
-  //   </div>
-  // );
-  return React.createElement('div' ,{clssName :'App'}, React.createElement( 'div' , null, 'React Library used'), <Some/> );
+   return (
+    <div className="App">
+        <h1>React First App</h1>
+        <Some/>
+    </div>
+  );
+  //return React.createElement('div' ,{clssName :'App'}, React.createElement( 'h1' , null, 'React Library used'), <Some/> );
  
 }
 
 function Some() {
-  // return(
-  //   <h1>Another Component React Project</h1>
-  // );
-  return React.createElement( 'h1' , null, 'Another React Component in This project !!!');
+  return(
+    <h1>Another Component React Project</h1>
+  );
+  //return React.createElement( 'div' , null, 'Another React Component in This project !!!');
+  //return React.createElement( 'div' , null, 'React.createElement( 'h1' , null,Another React Component in This project !!!'));
 }
 
 export default App;
