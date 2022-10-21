b3bcf1c6ba0dfb1679f9a92791090b8c5459537f
mahfuzur <mdmahfuazur7788@gamil.com>
2022-10-21 11:33:09 +0600

    JSX Syntax used

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import React from 'react';
 //JSF Code
 
 function App() {
-   return (
-    <div className="App">
-        <h1>React First App</h1>
-        <Some/>
-    </div>
-  );
+  //  return (
+  //   <div className="App">
+  //       <h1>React First App</h1>
+  //       <Some/>
+  //   </div>
+  // );
+  return React.createElement('div' ,{clssName :'App'}, React.createElement( 'div' , null, 'React Library used'), <Some/> );
  
 }
 
 function Some() {
-  return(
-    <h1>Another Component React Project</h1>
-  );
+  // return(
+  //   <h1>Another Component React Project</h1>
+  // );
+  return React.createElement( 'h1' , null, 'Another React Component in This project !!!');
 }
 
 export default App;
