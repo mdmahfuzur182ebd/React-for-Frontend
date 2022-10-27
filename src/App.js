b535759c2612fc69ef519264447255be14ede52f
mahfuzur <mdmahfuazur7788@gamil.com>
2022-10-27 10:31:27 +0600

    State of a Component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import Book from './components/Book';
 
 
 class App extends Component{
-  render() {
+  //state
+  //16.8
+
+   state = {
+    books : [
+      { bookname:"1984", writer :"George Orwell"},
+      { bookname:"The De Vinci code", writer :"Dan Brown"},
+      { bookname:"The Alchemist", writer :"Paulo Coelho"},
+    ],
+    otherBook: "I am some other  Props"
+    
+   };
+
+  //  constructor() {
+  //     super();
+  //     this.state = {}; 
+  //   }
+
+   render() {
    return (
       <div className="App">
-      <h1>Book list</h1>   
-      <Book bookname="1984" writer ="George Orwell"/>
-      <Book bookname="The De Vinci code " writer ="Dan Brown"/>
-      <Book name="The Alchemist" writer ="Paulo Coelho"/>
+          <h1>Book list</h1>   
+          <Book bookname={this.state.books[0].bookname} writer ={this.state.books[0].writer} />
+          <Book bookname={this.state.books[1].bookname} writer ={this.state.books[1].writer} />
+          <Book bookname={this.state.books[2].bookname} writer ={this.state.books[2].writer} />
       </div>
     );
   }
